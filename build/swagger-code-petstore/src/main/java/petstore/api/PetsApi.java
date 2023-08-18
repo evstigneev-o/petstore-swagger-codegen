2138b48442e6579c6ef226498268c1e0d53360d8
Oleg Evstigneev <o.evstigneev@citydrive.ru>
2023-08-18 17:52:51 +0300

    generate client

---
+++ build/swagger-code-petstore/src/main/java/petstore/api/PetsApi.java
@@ -0,0 +1,52 @@
+package petstore.api;
+
+import petstore.CollectionFormats.*;
+
+import retrofit2.Call;
+import retrofit2.http.*;
+
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+
+import petstore.model.Error;
+import petstore.model.Pet;
+import petstore.model.Pets;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public interface PetsApi {
+  /**
+   * Create a pet
+   * 
+   * @return Call&lt;Void&gt;
+   */
+  @POST("pets")
+  Call<Void> createPets();
+    
+
+  /**
+   * List all pets
+   * 
+   * @param limit How many items to return at one time (max 100) (optional)
+   * @return Call&lt;Pets&gt;
+   */
+  @GET("pets")
+  Call<Pets> listPets(
+    @retrofit2.http.Query("limit") Integer limit
+  );
+
+  /**
+   * Info for a specific pet
+   * 
+   * @param petId The id of the pet to retrieve (required)
+   * @return Call&lt;Pet&gt;
+   */
+  @GET("pets/{petId}")
+  Call<Pet> showPetById(
+    @retrofit2.http.Path("petId") String petId
+  );
+
+}
---
+++ build/swagger-code-petstore/src/test/java/petstore/api/PetsApiTest.java
@@ -0,0 +1,67 @@
+package petstore.api;
+
+import petstore.ApiClient;
+import petstore.model.Error;
+import petstore.model.Pet;
+import petstore.model.Pets;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * API tests for PetsApi
+ */
+public class PetsApiTest {
+
+    private PetsApi api;
+
+    @Before
+    public void setup() {
+        api = new ApiClient().createService(PetsApi.class);
+    }
+
+
+    /**
+     * Create a pet
+     *
+     * 
+     */
+    @Test
+    public void createPetsTest() {
+        // Void response = api.createPets();
+
+        // TODO: test validations
+    }
+
+    /**
+     * List all pets
+     *
+     * 
+     */
+    @Test
+    public void listPetsTest() {
+        Integer limit = null;
+        // Pets response = api.listPets(limit);
+
+        // TODO: test validations
+    }
+
+    /**
+     * Info for a specific pet
+     *
+     * 
+     */
+    @Test
+    public void showPetByIdTest() {
+        String petId = null;
+        // Pet response = api.showPetById(petId);
+
+        // TODO: test validations
+    }
+}
